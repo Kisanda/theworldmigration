508e3a0d13da70d83babc539187f1363ecf047f4
Kisanda Ranudaya <120372647+Kisanda@users.noreply.github.com>
2026-09-25 16:17:50 +0530

    deletye

--- src/content.js
+++
@@ -1,8 +0,0 @@
-import { createContext, useContext } from 'react';
-import schema from './content-schema.json';
-export { schema };
-export const defaults = Object.fromEntries(Object.values(schema).flatMap(section => section.fields.map(field => [field.key, field.default])));
-export const ContentContext = createContext(defaults);
-export const EditorContext = createContext(null);
-export const useContent = () => useContext(ContentContext);
-export const useEditor = () => useContext(EditorContext);
